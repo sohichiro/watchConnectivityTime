3ed5b9415cb05d072494a09c7c038e10d779a070
Sohichiro <sohnagao@gmail.com>
2015-12-01 09:57:22 +0900

    Initial Commit

---
+++ watchConnectibityTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchConnectibityTime
+//
+//  Created by 長尾聡一郎 on 2015/12/01.
+//  Copyright © 2015年 長尾聡一郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
